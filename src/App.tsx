03ebd30062e2c017e216a76e957f655fd2e156e1
Hubert Yu <hubyu9234@gmail.com>
2025-06-29 01:42:56 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { PDFViewer, pdf } from "@react-pdf/renderer";
+import LookBook from "./pdf/LookBook";
+import { Button } from "./components/ui/button";
+
+function App() {
+  const openPDFInNewTab = async () => {
+    const blob = await pdf(<LookBook />).toBlob();
+    const url = URL.createObjectURL(blob);
+    window.open(url, "_blank");
+  };
+
+  return (
+    <div>
+      <Button onClick={openPDFInNewTab}>Open PDF in New Tab</Button>
+    </div>
+  );
+}
+
+export default App;
