ca8dc30dd1d9257942a1b9d8284cca5c09899360
Hubert Yu <hubyu9234@gmail.com>
2025-06-29 02:06:03 -0700

    Basic functionailty test

--- src/pdf/LookBook.tsx
+++ src/pdf/LookBook.tsx
@@ -1,28 +1,47 @@
-import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import {
+  Page,
+  Text,
+  Image,
+  View,
+  Document,
+  StyleSheet,
+} from "@react-pdf/renderer";
 
 // Create styles
 const styles = StyleSheet.create({
   page: {
-    flexDirection: "row",
+    flexDirection: "column",
     backgroundColor: "#E4E4E4",
+    alignItems: "flex-start",
   },
   section: {
     margin: 10,
     padding: 10,
     flexGrow: 1,
   },
+  image: {
+    objectFit: "cover",
+    height: "100px",
+  },
 });
 
+type LookBookProps = {
+  section_one: string;
+  section_two: string;
+  image: string;
+};
+
 // Create Document Component
-const LookBook = () => (
+const LookBook = ({ section_one, section_two, image }: LookBookProps) => (
   <Document>
     <Page size="A4" style={styles.page}>
       <View style={styles.section}>
-        <Text>Section #1</Text>
+        <Text>{section_one}</Text>
       </View>
       <View style={styles.section}>
-        <Text>Section #2</Text>
+        <Text>{section_two}</Text>
       </View>
+      <Image style={styles.image} src={image} />
     </Page>
   </Document>
 );
